739dae0b29f15503c192142eb1b55d4a5735e85f
like5188 <260864866@qq.com>
2019-10-29 23:00:04 +0800

    DialogFragment对话框测试

--- app/src/main/java/com/like/common/sample/dialog/DialogFragment1.kt
+++ app/src/main/java/com/like/common/sample/dialog/DialogFragment1.kt
@@ -2,13 +2,11 @@ package com.like.common.sample.dialog
 
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import com.like.common.sample.R
 import com.like.common.sample.databinding.DialogFragment1Binding
-import org.jetbrains.anko.padding
 
 class DialogFragment1 : DialogFragment() {
     private var mBinding: DialogFragment1Binding? = null
@@ -26,6 +24,10 @@ class DialogFragment1 : DialogFragment() {
             }
             field = value
         }
+    private var mWidth = WindowManager.LayoutParams.WRAP_CONTENT
+    private var mHeight = WindowManager.LayoutParams.WRAP_CONTENT
+    private var mGravity = Gravity.CENTER
+    private var mDimAmount = 0.6f
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.dialog_fragment_1, container, false)
@@ -44,17 +46,28 @@ class DialogFragment1 : DialogFragment() {
         return mBinding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // 去除Dialog默认头部
+        dialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
+    }
+
     override fun onStart() {
         super.onStart()
-        // 设置对话框宽高，必须放到 onStart() 里面才有效。
-        dialog?.window?.attributes?.let {
-            it.width = WindowManager.LayoutParams.MATCH_PARENT
-            it.height = WindowManager.LayoutParams.WRAP_CONTENT
-//            it.gravity = Gravity.BOTTOM
-            dialog?.window?.attributes = it
+        // 设置 window 相关，必须放到 onStart() 里面才有效。
+        dialog?.window?.let {
+            // 宽高
+            val layoutParams = it.attributes
+            layoutParams.width = mWidth
+            layoutParams.height = mHeight
+            // 位置
+            layoutParams.gravity = mGravity
+            // 透明度
+            layoutParams.dimAmount = mDimAmount
+            it.attributes = layoutParams
+            // 设置背景透明，并去掉 dialog 默认的 padding ，默认是 24
+            it.setBackgroundDrawable(ColorDrawable())
         }
-        // 设置背景透明，并去掉 dialog 默认的 padding ，默认是 24
-//        dialog?.window?.setBackgroundDrawable(ColorDrawable())
     }
 
     fun setTitle(title: String) {
