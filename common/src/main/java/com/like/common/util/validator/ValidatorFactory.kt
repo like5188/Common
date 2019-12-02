91abb34c57346b2c7554f75810808d4debb9d015
like <like@adtech.com.cn>
2019-12-02 13:28:43 +0800

    完善注释

--- common/src/main/java/com/like/common/util/validator/ValidatorFactory.kt
+++ common/src/main/java/com/like/common/util/validator/ValidatorFactory.kt
@@ -4,8 +4,17 @@ import com.like.common.util.validator.validator.*
 
 /**
  * 验证器工厂，创建了默认实现的几个验证器。
- * 如果需要自定义其它验证器，请集成自[BaseValidator]，然后添加[AbstractRule]。
- * 其中[AbstractRule]默认实现了：[PredicateRule]、[RegExpRule]、[NotNullRule]、[NotEmptyRule]、[EmailRule]、[IDNumberRule]、[IPRule]、[PhoneRule]、[UrlRule]
+ * 如果需要自定义其它验证器，请集成自[BaseValidator]，然后添加[com.like.common.util.validator.rule.AbstractRule]。
+ * 其中[com.like.common.util.validator.rule.AbstractRule]的默认实现有：
+ * [com.like.common.util.validator.rule.PredicateRule]、
+ * [com.like.common.util.validator.rule.RegExpRule]、
+ * [com.like.common.util.validator.rule.NotNullRule]、
+ * [com.like.common.util.validator.rule.NotEmptyRule]、
+ * [com.like.common.util.validator.rule.EmailRule]、
+ * [com.like.common.util.validator.rule.IDNumberRule]、
+ * [com.like.common.util.validator.rule.IPRule]、
+ * [com.like.common.util.validator.rule.PhoneRule]、
+ * [com.like.common.util.validator.rule.UrlRule]
  */
 object ValidatorFactory {
     fun createPhoneValidator() = PhoneValidator()
