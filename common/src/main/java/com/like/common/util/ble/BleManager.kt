a4593f1c6032697eecde167212427d7cbf7a9f73
like <like@adtech.com.cn>
2019-12-05 14:25:50 +0800

    蓝牙模块优化

--- common/src/main/java/com/like/common/util/ble/BleManager.kt
+++ common/src/main/java/com/like/common/util/ble/BleManager.kt
@@ -19,26 +19,21 @@ import com.like.common.util.ble.scanstrategy.IScanStrategy
 import kotlinx.coroutines.CoroutineScope
 
 /**
- * 低功耗蓝牙服务，负责在后台实现蓝牙的连接，数据的发送接收
- * GATT，属性配置文件，定义数据的交互方式和含义。
- * 定义了三个概念：服务（Service）、特征（Characteristic）和描述（Descriptor）。
+ * 蓝牙是一种近距离无线通信技术。它的特性就是近距离通信，典型距离是 10 米以内，传输速度最高可达 24 Mbps，支持多连接，安全性高，非常适合用智能设备上。
+ * Android 4.3 开始，开始支持BLE功能，但只支持Central Mode（中心模式）
+ * Android 5.0开始，开始支持Peripheral Mode（外设模式）
  *
- * GATT 包含若干个 Service ，一个 Service 包含若干个 Characteristic，一个 Characteristic 可以包含若干 Descriptor。
+ * GATT，属性配置文件，定义数据的交互方式和含义。
+ * GATT 连接是独占的。也就是一个 BLE 外设同时只能被一个中心设备连接。一旦外设被连接，它就会马上停止广播，这样它就对其他设备不可见了。当设备断开，它又开始广播。中心设备和外设需要双向通信的话，唯一的方式就是建立 GATT 连接。
+ *
+ * GATT 定义了4个概念：配置文件（Profile）、服务（Service）、特征（Characteristic）和描述（Descriptor）。他们的关系是这样的：Profile 就是定义了一个实际的应用场景，一个 Profile包含若干个 Service，一个 Service 包含若干个 Characteristic，一个 Characteristic 可以包含若干 Descriptor。
+ * Profile 并不是实际存在于 BLE 外设上的，它只是一个被 Bluetooth SIG 或者外设设计者预先定义的 Service 的集合。例如心率Profile（Heart Rate Profile）就是结合了 Heart Rate Service 和 Device Information Service。
+ * Service 是把数据分成一个个的独立逻辑项，它包含一个或者多个 Characteristic。每个 Service 有一个 UUID 唯一标识。 UUID 有 16 bit 的，或者 128 bit 的。16 bit 的 UUID 是官方通过认证的，需要花钱购买，128 bit 是自定义的，这个就可以自己随便设置。官方通过了一些标准 Service，完整列表在这里。以 Heart Rate Service为例，可以看到它的官方通过 16 bit UUID 是  0x180D，包含 3 个 Characteristic：Heart Rate Measurement, Body Sensor Location 和 Heart Rate Control Point，并且定义了只有第一个是必须的，它是可选实现的。
+ * Characteristic， 它定义了数值和操作，包含一个Characteristic声明、Characteristic属性、值、值的描述(Optional)。通常我们讲的 BLE 通信，其实就是对 Characteristic 的读写或者订阅通知。比如在实际操作过程中，我对某一个Characteristic进行读，就是获取这个Characteristic的value。
  *
  * Service、Characteristic 还有 Descriptor 都是使用 UUID 唯一标示的。
- * 我们说的 BLE 通信，其实就是对 Characteristic 的读写或者订阅通知。
- *
- * UUID 是全局唯一标识，它是 128bit 的值，为了便于识别和阅读，一般标示程如下的形式，8-4-4-12 的16进制标示。
- * 我们也看到，UUID 有点太长了，在低功耗蓝牙中这种数据长度非常受限的情况下，使用起来肯定不方便，
- * 所以蓝牙又使用了所谓的 16 bit 或者 32 bit 的 UUID。其实本质上并没有什么 16bit 或者 32 bit UUID，
- * 蓝牙 SIG 定义了一个基础的UUID（Bluetooth Base UUID），形式如下：除了 XXXX 那几位意外，其他都是固定，
- * 所以说，其实 16 bit UUID 是对应了一个 128 bit 的 UUID。这样一来，UUID 就大幅减少了，
- * 例如 16 bit uuid 只有有限的 65536 个，所以 16 bit UUID 并不能随便使用。SIG 已经预先定义了一些 UUID，
- * 如果你想添加一些自己的 16 bit 的 UUID，可以花钱买。
- *
- * 从 Android 4.3 Jelly Bean，也就是 API 18 才开始支持低功耗蓝牙。
- * 这时支持 BLE 的 Central 模式，也就是我们在上面 GAP 中说的，Android 设备只能作为中心设备去连接其他设备。
- * 从 Android 5.0 开始才支持外设模式。
+ * UUID 是全局唯一标识，它是 128bit 的值，为了便于识别和阅读，一般以 “8位-4位-4位-4位-12位”的16进制标示，比如“12345678-abcd-1000-8000-123456000000”。
+ * 但是，128bit的UUID 太长，考虑到在低功耗蓝牙中，数据长度非常受限的情况，蓝牙又使用了所谓的 16 bit 或者 32 bit 的 UUID，形式如下：“0000XXXX-0000-1000-8000-00805F9B34FB”。除了 “XXXX” 那几位以外，其他都是固定，所以说，其实 16 bit UUID 是对应了一个 128 bit 的 UUID。这样一来，UUID 就大幅减少了，例如 16 bit UUID只有有限的 65536（16的四次方） 个。与此同时，因为数量有限，所以 16 bit UUID 并不能随便使用。蓝牙技术联盟已经预先定义了一些 UUID，我们可以直接使用，比如“00001011-0000-1000-8000-00805F9B34FB”就一个是常见于BLE设备中的UUID。当然也可以花钱定制自定义的UUID。
  *
  * 应用在使用蓝牙设备的时候必须要声明蓝牙权限 BLUETOOTH 需要这个权限才可以进行蓝牙通信，例如：请求连接、接受连接、和传输数据。
  * <uses-permission android:name="android.permission.BLUETOOTH" />
@@ -54,6 +49,8 @@ import kotlinx.coroutines.CoroutineScope
  * Android 从 4.3(API Level 18) 开始支持低功耗蓝牙，但是只支持作为中心设备（Central）模式，这就意味着 Android 设备只能主动扫描和链接其他外围设备（Peripheral）。从 Android 5.0(API Level 21) 开始两种模式都支持。
  *
  * BLE 所有回调函数都不是在主线程中的
+ *
+ * 接收通知：有两种方式可以接收通知，indicate和notify。indicate和notify的区别就在于，indicate是一定会收到数据，notify有可能会丢失数据。indicate底层封装了应答机制，如果没有收到中央设备的回应，会再次发送直至成功；而notify不会有central收到数据的回应，可能无法保证数据到达的准确性，优势是速度快。通常情况下，当外围设备需要不断地发送数据给APP的时候，比如血压计在测量过程中的压力变化，胎心仪在监护过程中的实时数据传输，这种频繁的情况下，优先考虑notify形式。当只需要发送很少且很重要的一条数据给APP的时候，优先考虑indicate形式。当然，从Android开发角度的出发，如果硬件放已经考虑了成熟的协议和发送方式，我们需要做的仅仅是根据其配置的数据发送方式进行相应的对接即可。
  *
  * @author like
  * @version 1.0
