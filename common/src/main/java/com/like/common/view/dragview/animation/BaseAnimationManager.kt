91b94acf37f1bac83c1beb77cd1c35b948757ee0
like <like@adtech.com.cn>
2020-03-04 16:59:29 +0800

    完善注释

--- common/src/main/java/com/like/common/view/dragview/animation/BaseAnimationManager.kt
+++ common/src/main/java/com/like/common/view/dragview/animation/BaseAnimationManager.kt
@@ -4,6 +4,12 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.AnimatorSet
 
+/**
+ * Animator: 提供创建属性动画的基类，基本不会直接使用这个类。
+ * ValueAnimator:属性动画用到的主要的时间引擎，负责计算各个帧的属性值，基本上其他属性动画都会直接或间接继承它；
+ * ObjectAnimator： ValueAnimator 的子类，对指定对象的属性执行动画。
+ * AnimatorSet：Animator 的子类，用于组合多个 Animator。
+ */
 abstract class BaseAnimationManager(val config: AnimationConfig) {
     private var isStart: Boolean = false
     private val animatorSet: AnimatorSet = AnimatorSet().apply {
@@ -12,11 +18,9 @@ abstract class BaseAnimationManager(val config: AnimationConfig) {
             override fun onAnimationStart(animation: Animator?) {
                 isStart = true
                 onStart()
-                super.onAnimationStart(animation)
             }
 
             override fun onAnimationEnd(animation: Animator?) {
-                super.onAnimationEnd(animation)
                 animation?.removeAllListeners()
                 isStart = false
                 onEnd()
