f23af62f0e57d2f1390c18965648099cdfbe81ee
like <like@adtech.com.cn>
2020-03-05 11:30:34 +0800

    重构dragview

--- common/src/main/java/com/like/common/view/dragview/view/DragViewActivity.kt
+++ common/src/main/java/com/like/common/view/dragview/view/DragViewActivity.kt
@@ -2,9 +2,9 @@ package com.like.common.view.dragview.view
 
 import android.os.Build
 import android.os.Bundle
-import androidx.core.content.ContextCompat
-import androidx.appcompat.app.AppCompatActivity
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.like.common.R
 import com.like.common.view.dragview.entity.DragInfo
 
@@ -22,10 +22,6 @@ class DragViewActivity : AppCompatActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            //getColor(int id)在API版本23时(Android 6.0)已然过时
-            //从这里我们可以看出，当API版本>=23时，使用ContextCompatApi23.getColor(context, id)方法，
-            //当API版本<23时，使用context.getResources().getColor(id)方法获取相应色值
-            //getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimary));
             window.statusBarColor = ContextCompat.getColor(this, R.color.common_transparent)
         }
 
