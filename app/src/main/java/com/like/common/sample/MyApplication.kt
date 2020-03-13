4efce6c542155d1c5de78ba3f03f0eb117521a11
like5188 <260864866@qq.com>
2020-03-13 21:51:22 +0800

    升级库

--- app/src/main/java/com/like/common/sample/MyApplication.kt
+++ app/src/main/java/com/like/common/sample/MyApplication.kt
@@ -1,7 +1,17 @@
 package com.like.common.sample
 
-import androidx.multidex.MultiDexApplication
+import android.content.Context
+import androidx.multidex.MultiDex
+import com.like.common.base.BaseApplication
 
-// 这里必须用 MultiDexApplication，因为 Android 19 时会报错：Unable to get provider androidx.lifecycle.ProcessLifecycleOwnerInitializr
-class MyApplication : MultiDexApplication() {
+// 这里可以在 Application 的 attachBaseContext 方法中添加初始化代码：MultiDex.install(this)，
+// 或者直接继承 MultiDexApplication。
+// 因为 Android 4.4 时会报错：Unable to get provider androidx.lifecycle.ProcessLifecycleOwnerInitializr
+class MyApplication : BaseApplication() {
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+    
 }
