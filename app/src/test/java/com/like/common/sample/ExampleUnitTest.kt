eb8561e33c1a631f940a020b5421dec376e54c31
like <260864866@qq.com>
2020-09-03 21:15:17 +0800

    完善

--- app/src/test/java/com/like/common/sample/ExampleUnitTest.kt
+++ app/src/test/java/com/like/common/sample/ExampleUnitTest.kt
@@ -1,7 +1,7 @@
 package com.like.common.sample
 
 import org.junit.Test
-import kotlin.reflect.KFunction
+import kotlin.reflect.full.declaredMemberFunctions
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -17,23 +17,42 @@ class ExampleUnitTest {
     companion object {
 
         fun start(a: String?, b: Int?) {
-            b(*::start.transformParamsToPairList(a, b).toTypedArray())
-            b("a" to a, "b" to b)
+            execute(*getParamPairs(a, b).toTypedArray())
         }
 
-        fun KFunction<*>.transformParamsToPairList(vararg params: Any?): List<Pair<String, Any?>> {
-            val paramsNames = parameters.map { it.name ?: "" }
-            if (paramsNames.isEmpty() || paramsNames.size != params.size) {
+        fun getParamPairs(vararg params: Any?): List<Pair<String, Any?>> {
+            val methodName = Thread.currentThread().stackTrace[2].methodName
+            val paramNames = getParamNames(methodName)
+            return transformParamsToPairList(paramNames, *params)
+        }
+
+        fun getParamNames(methodName: String): List<String> {
+            this::class.declaredMemberFunctions.forEach {
+                if (it.name == methodName) {
+                    val result = mutableListOf<String>()
+                    it.parameters.forEach {
+                        if (it.index > 0) {
+                            result.add(it.name ?: "")
+                        }
+                    }
+                    return result
+                }
+            }
+            return emptyList()
+        }
+
+        fun transformParamsToPairList(paramNames: List<String>, vararg params: Any?): List<Pair<String, Any?>> {
+            if (paramNames.isEmpty() || paramNames.size != params.size) {
                 return emptyList()
             }
             val result = mutableListOf<Pair<String, Any?>>()
-            paramsNames.forEachIndexed { index, s ->
+            paramNames.forEachIndexed { index, s ->
                 result.add(Pair(s, params[index]))
             }
             return result
         }
 
-        fun b(vararg params: Pair<String, Any?>) {
+        fun execute(vararg params: Pair<String, Any?>) {
             params.forEach {
                 println(it)
             }
