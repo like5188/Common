ce6b76bfcee779d4dd19926e457bf5c6f0bae5a3
like <260864866@qq.com>
2020-09-03 23:27:34 +0800

    完善

--- app/src/test/java/com/like/common/sample/ExampleUnitTest.kt
+++ app/src/test/java/com/like/common/sample/ExampleUnitTest.kt
@@ -1,7 +1,9 @@
 package com.like.common.sample
 
 import org.junit.Test
+import kotlin.reflect.KParameter
 import kotlin.reflect.full.declaredMemberFunctions
+import kotlin.reflect.jvm.javaType
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,44 +14,59 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         start("1", 2)
+//        start(0.1)
     }
 
     companion object {
 
+        fun start(c: Double?) {
+            execute(*getParamPairs(c))
+        }
+
+        fun start(a: Double?, b: Int?) {
+            execute(*getParamPairs(a, b))
+        }
+
         fun start(a: String?, b: Int?) {
-            execute(*getParamPairs(a, b).toTypedArray())
+            execute(*getParamPairs(a, b))
         }
 
-        fun getParamPairs(vararg params: Any?): List<Pair<String, Any?>> {
+        fun getParamPairs(vararg params: Any?): Array<Pair<String, Any?>> {
+            if (params.isNullOrEmpty()) {
+                return emptyArray()
+            }
             val methodName = Thread.currentThread().stackTrace[2].methodName
-            val paramNames = getParamNames(methodName)
-            return transformParamsToPairList(paramNames, *params)
+            val parameters = findMethod(methodName, *params) ?: return emptyArray()
+            return parameters.mapIndexed { index, kParameter -> Pair(kParameter.name ?: "", params[index]) }.toTypedArray()
         }
 
-        fun getParamNames(methodName: String): List<String> {
-            this::class.declaredMemberFunctions.forEach {
-                if (it.name == methodName) {
-                    val result = mutableListOf<String>()
-                    it.parameters.forEach {
-                        if (it.index > 0) {
-                            result.add(it.name ?: "")
-                        }
-                    }
-                    return result
+        /**
+         * 查找方法
+         * 1、方法名称相同
+         * 2、方法的形参数个数和实参个数相同
+         * 3、方法的每一个形参数据类型和实参数据类型都相同
+         *
+         * @return  找到方法就返回方法的形参列表
+         */
+        fun findMethod(methodName: String, vararg params: Any?): List<KParameter>? {
+            loop@ for (kFunction in this::class.declaredMemberFunctions) {
+                if (kFunction.name != methodName) {
+                    continue
                 }
+                val realParameters = kFunction.parameters.filter { it.kind == KParameter.Kind.VALUE }
+                if (realParameters.size != params.size) {
+                    continue
+                }
+                for (i in realParameters.indices) {
+                    val kParameter = realParameters[i]
+                    val param = params[i]
+                    if (kParameter.type.javaType.typeName != param?.javaClass?.typeName) {
+                        continue@loop
+                    }
+                }
+                return realParameters
             }
-            return emptyList()
-        }
-
-        fun transformParamsToPairList(paramNames: List<String>, vararg params: Any?): List<Pair<String, Any?>> {
-            if (paramNames.isEmpty() || paramNames.size != params.size) {
-                return emptyList()
-            }
-            val result = mutableListOf<Pair<String, Any?>>()
-            paramNames.forEachIndexed { index, s ->
-                result.add(Pair(s, params[index]))
-            }
-            return result
+            return null
         }
 
         fun execute(vararg params: Pair<String, Any?>) {
