b0185cbac0925f59299b4d942002a3e84f758978
like <like@adtech.com.cn>
2020-09-04 09:16:04 +0800

    完善例子

--- app/src/test/java/com/like/common/sample/ExampleUnitTest.kt
+++ app/src/test/java/com/like/common/sample/ExampleUnitTest.kt
@@ -13,50 +13,49 @@ import kotlin.reflect.jvm.javaType
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        start("1", 2)
-//        start(0.1)
+        start(0.1)
+        start(0.2, 2)
+        start("3", 3)
     }
 
     companion object {
 
         fun start(c: Double?) {
-            execute(*getParamPairs(c))
+            executeProxy(c)
         }
 
         fun start(a: Double?, b: Int?) {
-            execute(*getParamPairs(a, b))
+            executeProxy(a, b)
         }
 
         fun start(a: String?, b: Int?) {
             executeProxy(a, b)
         }
 
-        fun getParamPairs(vararg params: Any?): Array<Pair<String, Any?>> {
+        fun transformParamsToPairs(vararg params: Any?): Array<Pair<String, Any?>> {
             if (params.isNullOrEmpty()) {
                 return emptyArray()
             }
             val methodName = Thread.currentThread().stackTrace[3].methodName
-            val parameters = findMethod(methodName, *params) ?: return emptyArray()
+            val parameters = getParametersIfMethodFound(methodName, *params) ?: return emptyArray()
             return parameters.mapIndexed { index, kParameter -> Pair(kParameter.name ?: "", params[index]) }.toTypedArray()
         }
 
         /**
-         * 查找方法
-         * 1、方法名称相同
-         * 2、方法的形参数个数和实参个数相同
-         * 3、方法的每一个形参数据类型和实参数据类型都相同
-         *
-         * @return  找到方法就返回方法的形参列表
+         * 如果找到方法，就返回它的形参列表
          */
-        fun findMethod(methodName: String, vararg params: Any?): List<KParameter>? {
+        fun getParametersIfMethodFound(methodName: String, vararg params: Any?): List<KParameter>? {
             loop@ for (kFunction in this::class.declaredMemberFunctions) {
+                //方法名称相同
                 if (kFunction.name != methodName) {
                     continue
                 }
+                //方法的形参数个数和实参个数相同
                 val realParameters = kFunction.parameters.filter { it.kind == KParameter.Kind.VALUE }
                 if (realParameters.size != params.size) {
                     continue
                 }
+                //方法的每一个形参数据类型和实参数据类型都相同
                 for (i in realParameters.indices) {
                     val kParameter = realParameters[i]
                     val param = params[i]
@@ -70,7 +69,7 @@ class ExampleUnitTest {
         }
 
         fun executeProxy(vararg params: Any?) {
-            execute(*getParamPairs(*params))
+            execute(*transformParamsToPairs(*params))
         }
 
         fun execute(vararg params: Pair<String, Any?>) {
