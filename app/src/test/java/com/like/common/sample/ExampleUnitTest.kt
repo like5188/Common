15fabdca1f715a6cc4dee230777140bd1041f6bf
like <like@adtech.com.cn>
2020-09-04 10:38:08 +0800

    完善例子

--- app/src/test/java/com/like/common/sample/ExampleUnitTest.kt
+++ app/src/test/java/com/like/common/sample/ExampleUnitTest.kt
@@ -13,12 +13,12 @@ import kotlin.reflect.jvm.javaType
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-//        start(0.1)
+        start(0.1)
         start(0.2, 2)
-//        val caller = AAA()
-//        start(caller, "3", 3) {
-//
-//        }
+        val caller = AAA()
+        start(caller, "3", 3) {
+
+        }
     }
 
     companion object {
@@ -35,26 +35,20 @@ class ExampleUnitTest {
             startActivityForResultProxy(activityResultCaller, callback, a, b)
         }
 
+        /**
+         * 把参数转换成 "name" to name 这样的键值对
+         *
+         * @param actualParameters          实参（需要注入的参数）
+         * @param filterFormalParameters    过滤掉形参中的不需要注入的参数
+         */
         fun transformParametersToPairs(vararg actualParameters: Any?, filterFormalParameters: (List<KParameter>) -> List<KParameter>): Array<Pair<String, Any?>> {
             if (actualParameters.isNullOrEmpty()) {
                 return emptyArray()
             }
-            // 获取调用者方法名
-            val callerMethodName = Thread.currentThread().stackTrace[3].methodName
-            // 获取形参列表
-            val formalParameters = getFormalParametersIfMethodFound(callerMethodName, *actualParameters, filterFormalParameters = filterFormalParameters)
-                    ?: return emptyArray()
-            // 把形参列表和实参列表组合成Pair列表
-            return formalParameters.mapIndexed { index, kParameter -> Pair(kParameter.name ?: "", actualParameters[index]) }.toTypedArray()
-        }
-
-        /**
-         * 如果找到方法，就返回它的形参列表
-         */
-        fun getFormalParametersIfMethodFound(callerMethodName: String, vararg actualParameters: Any?, filterFormalParameters: (List<KParameter>) -> List<KParameter>): List<KParameter>? {
+            // 查找调用者方法
             loop@ for (kFunction in this::class.declaredMemberFunctions) {
                 //方法名称相同
-                if (kFunction.name != callerMethodName) {
+                if (kFunction.name != Thread.currentThread().stackTrace[3].methodName) {
                     continue
                 }
                 //方法的形参数个数和实参个数相同
@@ -71,9 +65,13 @@ class ExampleUnitTest {
                         continue@loop
                     }
                 }
-                return realFormalParameters
+
+                // 找到了方法，就把它的形参列表和实参列表组合成Pair列表
+                return realFormalParameters.mapIndexed { index, kParameter ->
+                    Pair(kParameter.name ?: "", actualParameters[index])
+                }.toTypedArray()
             }
-            return null
+            return emptyArray()
         }
 
         fun startActivityProxy(vararg actualParameters: Any?) {
