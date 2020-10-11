caaf8eff978455a791d52b40b79735aeb6cf525a
like <260864866@qq.com>
2020-10-11 20:50:14 +0800

    完善

--- common/src/main/java/com/like/common/util/Result.kt
+++ common/src/main/java/com/like/common/util/Result.kt
@@ -1,7 +1,6 @@
-package com.like.common.util.repository
+package com.like.common.util
 
 import androidx.lifecycle.LifecycleOwner
-import com.like.common.util.map
 import com.like.recyclerview.adapter.BaseAdapter
 import com.like.recyclerview.listener.OnItemClickListener
 import com.like.recyclerview.model.*
@@ -12,6 +11,62 @@ import com.like.recyclerview.ui.DefaultLoadMoreHeader
 import com.like.repository.RequestState
 import com.like.repository.RequestType
 import com.like.repository.Result
+
+//[com.like.repository.Result]相关的操作
+/**
+ * 绑定成功失败回调。
+ *
+ * @param onSuccess         成功回调
+ * @param onFailed          失败回调
+ */
+fun <ResultType> Result<ResultType>.bindResult(
+        lifecycleOwner: LifecycleOwner,
+        onSuccess: (ResultType?) -> Unit,
+        onFailed: ((RequestType, Throwable?) -> Unit)? = null
+) {
+    liveValue.observe(lifecycleOwner) {
+        val stateReport = liveState.value
+        if (stateReport?.state is RequestState.Success) {
+            onSuccess(it)
+        }
+    }
+    if (onFailed != null) {
+        liveState.observe(lifecycleOwner) { stateReport ->
+            val state = stateReport.state
+            val type = stateReport.type
+            if (state is RequestState.Failed) {
+                onFailed(type, state.throwable)
+            }
+        }
+    }
+}
+
+/**
+ * 初始化或者刷新时控制进度条的显示隐藏。
+ *
+ * @param show      初始化或者刷新开始时显示进度条
+ * @param hide      初始化或者刷新成功或者失败时隐藏进度条
+ */
+fun <ResultType> Result<ResultType>.bindProgress(
+        lifecycleOwner: LifecycleOwner,
+        show: () -> Unit,
+        hide: () -> Unit
+) {
+    liveState.observe(lifecycleOwner) { stateReport ->
+        val state = stateReport.state
+        val type = stateReport.type
+        if (type is RequestType.Initial || type is RequestType.Refresh) {
+            when (state) {
+                is RequestState.Running -> {
+                    show()
+                }
+                else -> {
+                    hide()
+                }
+            }
+        }
+    }
+}
 
 /**
  * 把 [Result] 与 [androidx.recyclerview.widget.RecyclerView] 进行绑定
@@ -26,11 +81,11 @@ import com.like.repository.Result
 fun <T : IRecyclerViewItem> Result<List<T>?>.bindRecyclerView(
         lifecycleOwner: LifecycleOwner,
         adapter: BaseAdapter,
-        emptyItem: IEmptyItem?,
-        errorItem: IErrorItem?,
-        loadMoreFooter: ILoadMoreFooter?,
-        loadMoreHeader: ILoadMoreHeader?,
-        listener: OnItemClickListener?
+        emptyItem: IEmptyItem? = null,
+        errorItem: IErrorItem? = null,
+        loadMoreFooter: ILoadMoreFooter? = null,
+        loadMoreHeader: ILoadMoreHeader? = null,
+        listener: OnItemClickListener? = null
 ) {
     liveState.observe(lifecycleOwner) { stateReport ->
         stateReport ?: return@observe
@@ -153,86 +208,4 @@ fun <T : IRecyclerViewItem> Result<List<T>?>.bindRecyclerView(
             }
         }
     }
-}
-
-/**
- * 把 [Result] 与 [androidx.recyclerview.widget.RecyclerView] 进行绑定，不分页
- * 包括空视图、错误视图、点击监听及Item数据的自动添加
- *
- * @param errorItem         失败时显示的视图。默认为：[DefaultErrorItem]
- * @param emptyItem         数据为空时显示的视图。默认为：[DefaultEmptyItem]
- * @param listener          item点击监听
- */
-fun <T : IRecyclerViewItem> Result<List<T>?>.bindRecyclerViewForNotPaging(
-        lifecycleOwner: LifecycleOwner,
-        adapter: BaseAdapter,
-        emptyItem: IEmptyItem? = DefaultEmptyItem(),
-        errorItem: IErrorItem? = DefaultErrorItem(),
-        listener: OnItemClickListener? = null
-) {
-    bindRecyclerView(
-            lifecycleOwner,
-            adapter,
-            emptyItem,
-            errorItem,
-            null,
-            null,
-            listener
-    )
-}
-
-/**
- * 把 [Result] 与 [androidx.recyclerview.widget.RecyclerView] 进行绑定，往后加载更多分页
- * 包括空视图、错误视图、往后加载更多视图、点击监听及Item数据的自动添加
- *
- * @param errorItem         失败时显示的视图。默认为：[DefaultErrorItem]
- * @param emptyItem         数据为空时显示的视图。默认为：[DefaultEmptyItem]
- * @param loadMoreFooter    往后加载更多的视图。默认为：[DefaultLoadMoreFooter]
- * @param listener          item点击监听
- */
-fun <T : IRecyclerViewItem> Result<List<T>?>.bindRecyclerViewForLoadAfterPaging(
-        lifecycleOwner: LifecycleOwner,
-        adapter: BaseAdapter,
-        emptyItem: IEmptyItem? = DefaultEmptyItem(),
-        errorItem: IErrorItem? = DefaultErrorItem(),
-        loadMoreFooter: ILoadMoreFooter? = DefaultLoadMoreFooter { this.retry() },
-        listener: OnItemClickListener? = null
-) {
-    bindRecyclerView(
-            lifecycleOwner,
-            adapter,
-            emptyItem,
-            errorItem,
-            loadMoreFooter,
-            null,
-            listener
-    )
-}
-
-/**
- * 把 [Result] 与 [androidx.recyclerview.widget.RecyclerView] 进行绑定，往前加载更多分页
- * 包括空视图、错误视图、往前加载更多视图、点击监听及Item数据的自动添加
- *
- * @param errorItem         失败时显示的视图。默认为：[DefaultErrorItem]
- * @param emptyItem         数据为空时显示的视图。默认为：[DefaultEmptyItem]
- * @param loadMoreHeader    往前加载更多的视图。默认为：[DefaultLoadMoreHeader]
- * @param listener          item点击监听
- */
-fun <T : IRecyclerViewItem> Result<List<T>?>.bindRecyclerViewForLoadBeforePaging(
-        lifecycleOwner: LifecycleOwner,
-        adapter: BaseAdapter,
-        emptyItem: IEmptyItem? = DefaultEmptyItem(),
-        errorItem: IErrorItem? = DefaultErrorItem(),
-        loadMoreHeader: ILoadMoreHeader? = DefaultLoadMoreHeader { this.retry() },
-        listener: OnItemClickListener? = null
-) {
-    bindRecyclerView(
-            lifecycleOwner,
-            adapter,
-            emptyItem,
-            errorItem,
-            null,
-            loadMoreHeader,
-            listener
-    )
 }
