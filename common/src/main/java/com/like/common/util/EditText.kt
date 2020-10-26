eb1afcc924095ecab7030f2f13e2e4d070ceaf23
like <like@adtech.com.cn>
2020-10-26 17:29:09 +0800

    完善防抖动搜索功能

--- common/src/main/java/com/like/common/util/EditText.kt
+++ common/src/main/java/com/like/common/util/EditText.kt
@@ -3,8 +3,7 @@ package com.like.common.util
 import android.util.Log
 import android.widget.EditText
 import androidx.core.widget.doAfterTextChanged
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.*
@@ -17,31 +16,30 @@ import kotlinx.coroutines.flow.*
  * @param search                    搜索的方法。参数为非空字符串。
  */
 @OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
-fun <T> EditText.search(
+suspend fun EditText.search(
         debounceTimeoutMillis: Long = 500L,
         filter: (String) -> Boolean = { true },
-        search: suspend (String) -> T
-): LiveData<T> {
+        search: (String) -> Unit
+) {
     // stateFlow 的使用方式类似于 LiveData，用于替代 ConflatedBroadcastChannel
     val stateFlow = MutableStateFlow<String?>(null)
     this.doAfterTextChanged {
         stateFlow.value = it?.toString()
     }
-    return stateFlow.debounce(debounceTimeoutMillis)
+    stateFlow.debounce(debounceTimeoutMillis)
             .filter {
                 return@filter !it.isNullOrEmpty() && filter(it)
             }
-            .flatMapLatest {
+            .onEach {
                 // 只显示最后一次搜索的结果，忽略之前的请求
                 // 网络请求，这里替换自己的实现即可
-                flow {
-                    emit(search(it!!))
-                }
+                search(it!!)
             }
             .catch { throwable ->
                 //  异常捕获
                 throwable.printStackTrace()
                 Log.e("EditText", "search error $throwable")
             }
-            .asLiveData()
+            .flowOn(Dispatchers.IO)
+            .collect()
 }
