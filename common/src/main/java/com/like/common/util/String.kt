a923ceb327bca76679f3d594a371976b25fe7407
like <like@adtech.com.cn>
2020-10-26 17:36:10 +0800

    完善防抖动搜索功能、String是否包含中文功能

--- common/src/main/java/com/like/common/util/String.kt
+++ common/src/main/java/com/like/common/util/String.kt
@@ -2,6 +2,9 @@ package com.like.common.util
 
 import java.util.regex.Pattern
 
+/**
+ * 删除字符串最后一个字符
+ */
 fun String?.deleteLast(): String? {
     if (this.isNullOrEmpty()) {
         return this
@@ -9,9 +12,24 @@ fun String?.deleteLast(): String? {
     return this.substring(0, this.length - 1)
 }
 
+/**
+ * 字符串是否包含中文汉字
+ */
 fun String?.containsChinese(): Boolean {
     if (this.isNullOrEmpty()) {
         return false
     }
-    return Pattern.compile("[\u4e00-\u9fa5]").matcher(this).find()
+    val regex = "[\u4e00-\u9fa5]"
+    return Pattern.compile(regex).matcher(this).find()
+}
+
+/**
+ * 字符串是否包含中文汉字或者中文标点
+ */
+fun String?.containsChineseAndPunctuation(): Boolean {
+    if (this.isNullOrEmpty()) {
+        return false
+    }
+    val regex = "[\u4E00-\u9FA5|\\！|\\，|\\。|\\（|\\）|\\《|\\》|\\“|\\”|\\？|\\：|\\；|\\【|\\】]"
+    return Pattern.compile(regex).matcher(this).find()
 }
