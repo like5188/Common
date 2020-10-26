995150b9c8adbf78b362e8a1bdd300bbc42744e5
like <like@adtech.com.cn>
2020-10-26 17:30:19 +0800

    完善防抖动搜索功能、String是否包含中文功能

--- common/src/main/java/com/like/common/util/String.kt
+++ common/src/main/java/com/like/common/util/String.kt
@@ -1,8 +1,17 @@
 package com.like.common.util
 
+import java.util.regex.Pattern
+
 fun String?.deleteLast(): String? {
     if (this.isNullOrEmpty()) {
         return this
     }
     return this.substring(0, this.length - 1)
+}
+
+fun String?.containsChinese(): Boolean {
+    if (this.isNullOrEmpty()) {
+        return false
+    }
+    return Pattern.compile("[\u4e00-\u9fa5]").matcher(this).find()
 }
