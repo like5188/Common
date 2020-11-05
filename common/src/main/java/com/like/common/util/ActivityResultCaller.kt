d3511fe2e309c7ba2ff59adeb136c083a7ad0847
like <260864866@qq.com>
2020-11-05 20:56:58 +0800

    完善ActivityResultCaller.kt

--- common/src/main/java/com/like/common/util/ActivityResultCaller.kt
+++ common/src/main/java/com/like/common/util/ActivityResultCaller.kt
@@ -8,6 +8,7 @@ import androidx.activity.result.ActivityResultCaller
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlin.coroutines.resume
@@ -22,55 +23,81 @@ val ActivityResultCaller.context: Context
         }
     }
 
-suspend inline fun <reified T : Activity> ActivityResultCaller.startActivityForResult(vararg params: Pair<String, Any?>): Intent? = withContext(Dispatchers.Main) {
-    suspendCoroutine { cont ->
-        val intent = context.createIntent<T>(*params)
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            cont.resume(
-                    if (it.resultCode == Activity.RESULT_OK) {
-                        it.data
-                    } else {
-                        null
-                    }
-            )
-        }.launch(intent)
+val ActivityResultCaller.lifecycleOwner: LifecycleOwner
+    get() {
+        return when (this) {
+            is ComponentActivity -> this
+            is Fragment -> this
+            else -> throw IllegalStateException("$this must be androidx.activity.ComponentActivity or androidx.fragment.app.Fragment")
+        }
     }
+
+suspend inline fun <reified T : Activity> ActivityResultCaller.startActivityForResult(vararg params: Pair<String, Any?>): Intent? {
+    val intent = context.createIntent<T>(*params)
+    return startActivityForResult(intent)
+}
+
+inline fun <reified T : Activity> ActivityResultCaller.startActivityForResult(vararg params: Pair<String, Any?>, crossinline callback: (Intent?) -> Unit) {
+    val intent = context.createIntent<T>(*params)
+    startActivityForResult(intent, callback)
 }
 
 suspend fun ActivityResultCaller.startActivityForResult(intent: Intent): Intent? = withContext(Dispatchers.Main) {
     suspendCoroutine { cont ->
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            cont.resume(
-                    if (it.resultCode == Activity.RESULT_OK) {
-                        it.data
-                    } else {
-                        null
-                    }
-            )
-        }.launch(intent)
+        startActivityForResult(intent) {
+            cont.resume(it)
+        }
     }
+}
+
+inline fun ActivityResultCaller.startActivityForResult(intent: Intent, crossinline callback: (Intent?) -> Unit) {
+    registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        callback(if (it.resultCode == Activity.RESULT_OK) {
+            it.data
+        } else {
+            null
+        })
+    }.launch(intent)
 }
 
 suspend fun ActivityResultCaller.startIntentSenderForResult(intentSenderRequest: IntentSenderRequest): Boolean = withContext(Dispatchers.Main) {
     suspendCoroutine { cont ->
-        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) {
-            cont.resume(it.resultCode == Activity.RESULT_OK)
-        }.launch(intentSenderRequest)
+        startIntentSenderForResult(intentSenderRequest) {
+            cont.resume(it)
+        }
     }
+}
+
+inline fun ActivityResultCaller.startIntentSenderForResult(intentSenderRequest: IntentSenderRequest, crossinline callback: (Boolean) -> Unit) {
+    registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) {
+        callback(it.resultCode == Activity.RESULT_OK)
+    }.launch(intentSenderRequest)
 }
 
 suspend fun ActivityResultCaller.requestPermission(permission: String): Boolean = withContext(Dispatchers.Main) {
     suspendCoroutine { cont ->
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        requestPermission(permission) {
             cont.resume(it)
-        }.launch(permission)
+        }
     }
+}
+
+inline fun ActivityResultCaller.requestPermission(permission: String, crossinline callback: (Boolean) -> Unit) {
+    registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        callback(it)
+    }.launch(permission)
 }
 
 suspend fun ActivityResultCaller.requestPermissions(vararg permissions: String): Boolean = withContext(Dispatchers.Main) {
     suspendCoroutine { cont ->
-        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
-            cont.resume(it.values.all { it })
-        }.launch(permissions)
+        requestPermissions(*permissions) {
+            cont.resume(it)
+        }
     }
+}
+
+inline fun ActivityResultCaller.requestPermissions(vararg permissions: String, crossinline callback: (Boolean) -> Unit) {
+    registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+        callback(it.values.all { it })
+    }.launch(permissions)
 }
