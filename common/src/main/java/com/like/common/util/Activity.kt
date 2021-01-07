b0d1cc73d6034f9a356f736d279da9b71667385f
tangwanjin <471634484@qq.com>
2021-01-07 17:13:22 +0800

    完善Activity.kt

--- common/src/main/java/com/like/common/util/Activity.kt
+++ common/src/main/java/com/like/common/util/Activity.kt
@@ -1,23 +1,16 @@
 package com.like.common.util
 
 import android.app.Activity
+import android.app.Application
 import android.content.Context
-import com.like.common.base.BaseApplication
-
-/**
- * 通过[BaseApplication]启动[Activity]
- */
-inline fun <reified T : Activity> startActivityByApplication(vararg params: Pair<String, Any?>) {
-    BaseApplication.sInstance.apply {
-        val intent = createIntent<T>(*params).newTask()
-        startActivity(intent)
-    }
-}
 
 /**
  * 通过[Context]启动[Activity]
  */
 inline fun <reified T : Activity> Context.startActivity(vararg params: Pair<String, Any?>) {
     val intent = createIntent<T>(*params)
+    if (this is Application) {
+        intent.newTask()
+    }
     startActivity(intent)
 }
