05f516c485f907054c8da1eaccd90c493c386dcc
like <260864866@qq.com>
2021-04-01 21:59:01 +0800

    完善界面跳转

--- common/src/main/java/com/like/common/util/ActivityResult.kt
+++ common/src/main/java/com/like/common/util/ActivityResult.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import androidx.activity.ComponentActivity
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.MainThread
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
@@ -12,52 +15,95 @@ import kotlin.coroutines.suspendCoroutine
 object ActivityResult {
     class RequestPermission(val activity: ComponentActivity) {
         private var continuation: Continuation<Boolean>? = null
+        private var callback: ((Boolean) -> Unit)? = null
         private val launcher = activity.registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            callback?.invoke(it)
             continuation?.resume(it)
         }
 
-        suspend fun requestPermission(permission: String): Boolean = suspendCoroutine {
-            continuation = it
+        suspend fun requestPermission(permission: String): Boolean = withContext(Dispatchers.Main) {
+            suspendCoroutine {
+                continuation = it
+                launcher.launch(permission)
+            }
+        }
+
+        @MainThread
+        fun requestPermission(permission: String, callback: (Boolean) -> Unit) {
+            this.callback = callback
             launcher.launch(permission)
         }
     }
 
     class RequestPermissions(val activity: ComponentActivity) {
         private var continuation: Continuation<Boolean>? = null
+        private var callback: ((Boolean) -> Unit)? = null
         private val launcher = activity.registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
-            continuation?.resume(it.values.all { it })
+            val result = it.values.all { it }
+            callback?.invoke(result)
+            continuation?.resume(result)
         }
 
-        suspend fun requestPermissions(vararg permissions: String): Boolean = suspendCoroutine {
-            continuation = it
+        suspend fun requestPermissions(vararg permissions: String): Boolean = withContext(Dispatchers.Main) {
+            suspendCoroutine {
+                continuation = it
+                launcher.launch(permissions)
+            }
+        }
+
+        @MainThread
+        fun requestPermissions(vararg permissions: String, callback: (Boolean) -> Unit) {
+            this.callback = callback
             launcher.launch(permissions)
         }
     }
 
     class StartActivityForResult(val activity: ComponentActivity) {
         private var continuation: Continuation<Intent?>? = null
+        private var callback: ((Intent?) -> Unit)? = null
         private val launcher = activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            continuation?.resume(if (it.resultCode == Activity.RESULT_OK) {
+            val intent = if (it.resultCode == Activity.RESULT_OK) {
                 it.data
             } else {
                 null
-            })
+            }
+            callback?.invoke(intent)
+            continuation?.resume(intent)
         }
 
-        suspend fun startActivityForResult(intent: Intent): Intent? = suspendCoroutine {
-            continuation = it
+        suspend fun startActivityForResult(intent: Intent): Intent? = withContext(Dispatchers.Main) {
+            suspendCoroutine {
+                continuation = it
+                launcher.launch(intent)
+            }
+        }
+
+        @MainThread
+        fun startActivityForResult(intent: Intent, callback: (Intent?) -> Unit) {
+            this.callback = callback
             launcher.launch(intent)
         }
     }
 
     class StartIntentSenderForResult(val activity: ComponentActivity) {
         private var continuation: Continuation<Boolean>? = null
+        private var callback: ((Boolean) -> Unit)? = null
         private val launcher = activity.registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) {
-            continuation?.resume(it.resultCode == Activity.RESULT_OK)
+            val result = it.resultCode == Activity.RESULT_OK
+            callback?.invoke(result)
+            continuation?.resume(result)
         }
 
-        suspend fun startIntentSenderForResult(intentSenderRequest: IntentSenderRequest): Boolean = suspendCoroutine {
-            continuation = it
+        suspend fun startIntentSenderForResult(intentSenderRequest: IntentSenderRequest): Boolean = withContext(Dispatchers.Main) {
+            suspendCoroutine {
+                continuation = it
+                launcher.launch(intentSenderRequest)
+            }
+        }
+
+        @MainThread
+        fun startIntentSenderForResult(intentSenderRequest: IntentSenderRequest, callback: (Boolean) -> Unit) {
+            this.callback = callback
             launcher.launch(intentSenderRequest)
         }
     }
