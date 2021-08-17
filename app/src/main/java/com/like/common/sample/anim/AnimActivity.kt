f761955492cd03228cc8e1c1b4bd245887de6a44
like5188 <260864866@qq.com>
2021-08-17 16:54:00 +0800

    添加动画例子

--- app/src/main/java/com/like/common/sample/anim/AnimActivity.kt
+++ app/src/main/java/com/like/common/sample/anim/AnimActivity.kt
@@ -1,8 +1,6 @@
 package com.like.common.sample.anim
 
-import android.animation.Keyframe
-import android.animation.ObjectAnimator
-import android.animation.PropertyValuesHolder
+import android.animation.*
 import android.graphics.Color
 import android.os.Bundle
 import android.view.Gravity
@@ -30,29 +28,50 @@ class AnimActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding.btn.setOnClickListener {
-            // 关键帧动画
-            val kf0 = Keyframe.ofFloat(0f, 0f)
-            val kf1 = Keyframe.ofFloat(.5f, 360f)
-            val kf2 = Keyframe.ofFloat(1f, 0f)
-            val pvhRotation = PropertyValuesHolder.ofKeyframe("rotation", kf0, kf1, kf2)
-            ObjectAnimator.ofPropertyValuesHolder(mBinding.btn, pvhRotation).apply {
-                duration = 5000
+//            // 关键帧动画
+//            val kf0 = Keyframe.ofFloat(0f, 0f)
+//            val kf1 = Keyframe.ofFloat(.5f, 360f)
+//            val kf2 = Keyframe.ofFloat(1f, 0f)
+//            val pvhRotation = PropertyValuesHolder.ofKeyframe("rotation", kf0, kf1, kf2)
+//            ObjectAnimator.ofPropertyValuesHolder(mBinding.btn, pvhRotation).apply {
+//                duration = 5000
+//                start()
+//            }
+//
+//            // 多个属性动画：三种方式
+//            // 1、一个 ObjectAnimator
+//            val pvhX = PropertyValuesHolder.ofFloat("x", 50f)
+//            val pvhY = PropertyValuesHolder.ofFloat("y", 100f)
+//            ObjectAnimator.ofPropertyValuesHolder(mBinding.btn, pvhX, pvhY).start()
+//            // 2、多个 ObjectAnimator
+//            val animX = ObjectAnimator.ofFloat(mBinding.btn, "x", 0f)
+//            val animY = ObjectAnimator.ofFloat(mBinding.btn, "y", 0f)
+//            AnimatorSet().apply {
+//                playTogether(animX, animY)
+//                start()
+//            }
+//            // 3、ViewPropertyAnimator
+//            mBinding.btn.animate().translationX(100f).translationY(200f)
+//
+//            mBinding.llContainer.addView(TextView(this).apply {
+//                text = "${i++}"
+//                setBackgroundColor(Color.GRAY)
+//                gravity = Gravity.CENTER
+//                layoutParams =
+//                    LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT).apply {
+//                        topMargin = 10.dp
+//                    }
+//                setPadding(10.dp, 10.dp, 10.dp, 10.dp)
+//                setOnClickListener {
+//                    mBinding.llContainer.removeView(this)
+//                }
+//            })
+
+            // 使用xml中的属性动画
+            (AnimatorInflater.loadAnimator(this, R.animator.animator) as AnimatorSet).apply {
+                setTarget( mBinding.btn)
                 start()
             }
-
-            mBinding.llContainer.addView(TextView(this).apply {
-                text = "${i++}"
-                setBackgroundColor(Color.GRAY)
-                gravity = Gravity.CENTER
-                layoutParams =
-                    LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT).apply {
-                        topMargin = 10.dp
-                    }
-                setPadding(10.dp, 10.dp, 10.dp, 10.dp)
-                setOnClickListener {
-                    mBinding.llContainer.removeView(this)
-                }
-            })
         }
     }
 
