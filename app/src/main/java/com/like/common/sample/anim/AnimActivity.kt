2cc1a498fa81fb2a6ff7b112eeb8c92060ce3f4f
like5188 <260864866@qq.com>
2021-08-17 16:19:39 +0800

    添加动画例子

--- app/src/main/java/com/like/common/sample/anim/AnimActivity.kt
+++ app/src/main/java/com/like/common/sample/anim/AnimActivity.kt
@@ -1,9 +1,11 @@
 package com.like.common.sample.anim
 
+import android.animation.Keyframe
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.graphics.Color
 import android.os.Bundle
 import android.view.Gravity
-import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -27,22 +29,31 @@ class AnimActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding
-    }
+        mBinding.btn.setOnClickListener {
+            // 关键帧动画
+            val kf0 = Keyframe.ofFloat(0f, 0f)
+            val kf1 = Keyframe.ofFloat(.5f, 360f)
+            val kf2 = Keyframe.ofFloat(1f, 0f)
+            val pvhRotation = PropertyValuesHolder.ofKeyframe("rotation", kf0, kf1, kf2)
+            ObjectAnimator.ofPropertyValuesHolder(mBinding.btn, pvhRotation).apply {
+                duration = 5000
+                start()
+            }
 
-    fun add(view: View) {
-        mBinding.llContainer.addView(TextView(this).apply {
-            text = "${i++}"
-            setBackgroundColor(Color.GRAY)
-            gravity = Gravity.CENTER
-            layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT).apply {
-                topMargin = 10.dp
-            }
-            setPadding(10.dp, 10.dp, 10.dp, 10.dp)
-            setOnClickListener {
-                mBinding.llContainer.removeView(this)
-            }
-        })
+            mBinding.llContainer.addView(TextView(this).apply {
+                text = "${i++}"
+                setBackgroundColor(Color.GRAY)
+                gravity = Gravity.CENTER
+                layoutParams =
+                    LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT).apply {
+                        topMargin = 10.dp
+                    }
+                setPadding(10.dp, 10.dp, 10.dp, 10.dp)
+                setOnClickListener {
+                    mBinding.llContainer.removeView(this)
+                }
+            })
+        }
     }
 
 }
