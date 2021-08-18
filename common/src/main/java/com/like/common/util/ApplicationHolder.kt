41151349c28d3b11c1c0df9367a4f70465010c72
like5188 <260864866@qq.com>
2021-08-18 15:27:40 +0800

    完善

--- common/src/main/java/com/like/common/util/ApplicationHolder.kt
+++ common/src/main/java/com/like/common/util/ApplicationHolder.kt
@@ -5,6 +5,7 @@ import android.app.Application
 import android.content.pm.ApplicationInfo
 import android.os.Bundle
 import java.util.*
+import kotlin.system.exitProcess
 
 /**
  * 持有 [Application] 实例，并对 [Activity] 进行了管理。
@@ -91,7 +92,7 @@ object ApplicationHolder {
     fun exitApp() {
         // 关闭所有界面，避免后一句代码执行后，如果栈中还有界面，会重启进程并启动下一个界面，导致不能彻底退出应用。
         finishAllActivities()
-        System.exit(0)
+        exitProcess(0)
     }
 
     /**
