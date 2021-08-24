bc55bf7bb791c0af52e3dfc1da4122a4a0a3dbf7
like5188 <260864866@qq.com>
2021-08-24 17:15:51 +0800

    完善存储工具

--- common/src/main/java/com/like/common/util/storage/external/ExternalStoragePublicUtils.kt
+++ common/src/main/java/com/like/common/util/storage/external/ExternalStoragePublicUtils.kt
@@ -41,10 +41,14 @@ import java.util.concurrent.TimeUnit
  * /storage/emulated/(0/1/...)/xxx
  * 其中 xxx 对应：媒体文件(MediaStore.Images、MediaStore.Video、MediaStore.Audio)、其它文件(MediaStore.Downloads)
  *
+ * Android10以下：
+ * 1、Environment.getExternalStorageDirectory() 需要申请存储权限：<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" android:maxSdkVersion="28" />
+ * 2、SAF 不需要权限
+ *
+ * Android10及其以上：
  * 权限：
  * 媒体文件：
- *      Android10以下：需要申请存储权限：<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" android:maxSdkVersion="28" />
- *      Android10以上：访问其它应用或者自己的旧版本应用的“媒体文件”时需要申请 READ_EXTERNAL_STORAGE 权限。
+ *      访问其它应用或者自己的旧版本应用的“媒体文件”时需要申请 READ_EXTERNAL_STORAGE 权限。
  *          当以 Android 10 或更高版本为目标平台的应用启用了分区存储时，系统会将每个媒体文件归因于一个应用，这决定了应用在未请求任何存储权限时可以访问的文件。每个文件只能归因于一个应用。因此，如果您的应用创建的媒体文件存储在照片、视频或音频文件媒体集合中，应用便可以访问该文件。
  *          但是，如果用户卸载并重新安装您的应用，您必须请求 READ_EXTERNAL_STORAGE 才能访问应用最初创建的文件。此权限请求是必需的，因为系统认为文件归因于以前安装的应用版本，而不是新安装的版本。
  *          MediaStore数据库增加owner_package_name字段记录文件属于哪个应用， 应用卸载后owner_package_name字段会置空，也就是说，卸载重装后，之前创建的文件，已不属于应用创建的了，需要相关存储权限才能再次读写
@@ -59,6 +63,7 @@ import java.util.concurrent.TimeUnit
  *      Android11：如果应用具有 READ_EXTERNAL_STORAGE 权限，则可以使用文件直接路径去访问媒体，但是应用的性能会略有下降，还是推荐使用 MediaStore API。
  * 注意：如果您不希望媒体扫描程序发现您的文件，请在特定于应用的目录中添加名为 .nomedia 的空文件（请注意文件名中的句点前缀）。这可以防止媒体扫描程序读取您的媒体文件并通过 MediaStore API 将它们提供给其他应用。
  * 2、其它文件：Storage Access Framework (不需要申请存储权限) Android 4.4（API 级别 19）引入，由用户自己通过系统选择器来操作文件。
+ *
  *
  * 按照分区存储的规范，将用户数据(例如图片、视频、音频等)保存在公共目录，把应用数据保存在私有目录
  *
