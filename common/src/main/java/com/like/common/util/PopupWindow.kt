408eb6a7203cfb2598f3b829adc2a9395c5d3485
like5188 <260864866@qq.com>
2021-11-11 10:02:57 +0800

    完善PopupWindow.kt

--- common/src/main/java/com/like/common/util/PopupWindow.kt
+++ common/src/main/java/com/like/common/util/PopupWindow.kt
@@ -1,20 +1,21 @@
 package com.like.common.util
 
-import android.graphics.drawable.ColorDrawable
 import android.view.View
 import android.widget.PopupWindow
 
 /**
  * 以 View 为内容视图来创建一个 PopupWindow。
+ *
+ * @param isFocusable   是否响应手机返回键或者点击外部区域消失
  */
-fun View.createPopupWindow(): PopupWindow {
-    this.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+fun View.toPopupWindow(width: Int = 0, height: Int = 0, isFocusable: Boolean = true): PopupWindow {
+    var realWidth = width
+    var realHeight = height
+    if (realWidth == 0 || realHeight == 0) {
+        this.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+        realWidth = this.measuredWidth
+        realHeight = this.measuredHeight
+    }
     // 这里必须设置指定的宽高，否则showAsDropDown默认只会向下弹出显示，这种情况有个最明显的缺点就是：弹窗口可能被屏幕截断，显示不全。
-    return PopupWindow(this, this.measuredWidth, this.measuredHeight)
-            .apply {
-                // 必须同时设置下面三个属性，才能在点击外部区域或者返回键时，关闭PopupWindow
-                setBackgroundDrawable(ColorDrawable(0x00000000))
-                isOutsideTouchable = true
-                isFocusable = true
-            }
+    return PopupWindow(this, realWidth, realHeight).apply { this.isFocusable = isFocusable }
 }
