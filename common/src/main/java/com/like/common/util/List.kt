6e2d43122fd1bd931750a4c8fb098cef226c92ed
like5188 <260864866@qq.com>
2021-11-24 16:58:11 +0800

    完善List.kt，增加深拷贝方法

--- common/src/main/java/com/like/common/util/List.kt
+++ common/src/main/java/com/like/common/util/List.kt
@@ -1,5 +1,10 @@
 package com.like.common.util
 
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+
 inline fun <T, reified V> List<T>?.map(): List<V> {
     val result = mutableListOf<V>()
     this?.forEach {
@@ -8,4 +13,19 @@ inline fun <T, reified V> List<T>?.map(): List<V> {
         }
     }
     return result
+}
+
+/**
+ * 对集合进行深拷贝
+ * 注意：泛型类必须要实现 Serializable 接口
+ */
+@Suppress("UNCHECKED_CAST")
+fun <T> List<T>.deepCopy(): List<T> {
+    val baos = ByteArrayOutputStream()
+    ObjectOutputStream(baos).use { oos ->
+        oos.writeObject(this)
+    }
+    return ObjectInputStream(ByteArrayInputStream(baos.toByteArray())).use { ois ->
+        ois.readObject()
+    } as List<T>
 }
