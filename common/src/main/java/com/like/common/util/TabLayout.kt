674f2c403060339d1b852fe5094cbb916ba50034
like5188 <260864866@qq.com>
2022-03-19 12:11:45 +0800

    TabLayout.kt优化

---
+++ common/src/main/java/com/like/common/util/TabLayout.kt
@@ -0,0 +1,21 @@
+package com.like.common.util
+
+import android.widget.FrameLayout
+import androidx.appcompat.widget.AppCompatTextView
+import com.google.android.material.tabs.TabLayout
+import com.like.common.view.BadgeView
+
+fun TabLayout.Tab.getTextView(): AppCompatTextView? {
+    return view.getChildAt(1) as? AppCompatTextView
+}
+
+fun TabLayout.Tab.setBadgeView(): BadgeView? {
+    val textView = getTextView() ?: return null
+    return BadgeView(textView.context).apply {
+        setTargetView(textView)
+    }
+}
+
+fun TabLayout.Tab.getBadgeView(): BadgeView? {
+    return (view.getChildAt(1) as? FrameLayout)?.getChildAt(1) as? BadgeView
+}
