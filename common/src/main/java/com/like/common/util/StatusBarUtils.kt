692df74a5380e12ebb793b29ef319e41d5b3bafa
like5188 <260864866@qq.com>
2022-03-24 20:22:19 +0800

    StatusBarUtils.kt优化

--- common/src/main/java/com/like/common/util/StatusBarUtils.kt
+++ common/src/main/java/com/like/common/util/StatusBarUtils.kt
@@ -8,66 +8,60 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 
-
-/**
- * 沉浸工具类
+/*
+ * 状态栏工具类
  *
  * @author like
- * @version 1.0
- * created on 2017/5/9 17:30
+ * created on 2022/03/23
  */
-object StatusBarUtils {
 
-    /**
-     * 设置状态栏为透明
-     * @param dark  是否设置状态栏中的文字、图标颜色为暗色，只在 api>=23 时有效。
-     */
-    fun setStatusBarTransparent(activity: Activity?, dark: Boolean = false) {
-        activity ?: return
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            // 5.x开始需要把颜色设置透明，否则导航栏会呈现系统默认的浅灰色
-            activity.window.apply {
-                clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-                addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-                // 两个 flag 要结合使用，表示让应用的主体内容占用系统状态栏的空间
-                var option = decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                if (dark && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    option = option or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-                }
-                decorView.systemUiVisibility = option
-                statusBarColor = Color.TRANSPARENT
+/**
+ * 设置透明的状态栏
+ * @param dark  是否设置状态栏中的文字、图标颜色为暗色，只在 api>=23 时有效。
+ */
+fun Activity.setTransparentStatusBar(dark: Boolean = false) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        // 5.x开始需要把颜色设置透明，否则导航栏会呈现系统默认的浅灰色
+        window.apply {
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            // 两个 flag 要结合使用，表示让应用的主体内容占用系统状态栏的空间
+            var option = decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            if (dark && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                option = option or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
             }
-        } else {
-            activity.window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            decorView.systemUiVisibility = option
+            statusBarColor = Color.TRANSPARENT
         }
+    } else {
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
     }
-
-    /**
-     * 对[view]增加状态栏高度的[paddingTop]
-     * 注意：如果[view]是[RelativeLayout]，那么增加[paddingTop]会影响到使用了[layout_centerVertical]、[layout_centerInParent]这两个在垂直方向上居中的属性的子view。
-     * 如果它的子view使用了这两个属性，那么需要自行处理，最好再包裹一层。
-     */
-    fun fitStatusBarHeight(view: View) {
-        val layoutParams = view.layoutParams
-        if (layoutParams.height != ViewGroup.LayoutParams.WRAP_CONTENT &&
-            layoutParams.height != ViewGroup.LayoutParams.MATCH_PARENT
-        ) {// 如果是固定高度的控件，则需要改变它的高度，为它增加一个状态栏高度。
-            layoutParams.height += getStatusBarHeight(view.context)
-        }
-        view.setPadding(
-            view.paddingLeft,
-            view.paddingTop + getStatusBarHeight(view.context),
-            view.paddingRight,
-            view.paddingBottom
-        )
-    }
-
-    /**
-     * 获得状态栏高度
-     */
-    fun getStatusBarHeight(context: Context): Int =
-        context.resources.getDimensionPixelSize(
-            context.resources.getIdentifier("status_bar_height", "dimen", "android")
-        )
-
 }
+
+/**
+ * 对[view]增加状态栏高度的[paddingTop]
+ * 注意：如果[view]是[RelativeLayout]，那么增加[paddingTop]会影响到使用了[layout_centerVertical]、[layout_centerInParent]这两个在垂直方向上居中的属性的子view。
+ * 如果它的子view使用了这两个属性，那么需要自行处理，最好再包裹一层。
+ */
+fun View.fitStatusBar() {
+    if (layoutParams.height != ViewGroup.LayoutParams.WRAP_CONTENT &&
+        layoutParams.height != ViewGroup.LayoutParams.MATCH_PARENT
+    ) {// 如果是固定高度的控件，则需要改变它的高度，为它增加一个状态栏高度。
+        layoutParams.height += context.statusBarHeight
+    }
+    setPadding(
+        paddingLeft,
+        paddingTop + context.statusBarHeight,
+        paddingRight,
+        paddingBottom
+    )
+}
+
+/**
+ * 状态栏高度
+ */
+val Context.statusBarHeight: Int
+    get() {
+        return resources.getDimensionPixelSize(resources.getIdentifier("status_bar_height", "dimen", "android"))
+    }
+
