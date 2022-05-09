5a457ff346c88fbf85afef1b02ef374147c69035
like5188 <260864866@qq.com>
2022-05-09 16:35:10 +0800

    完善例子

--- app/src/main/java/com/like/common/sample/fragmenttest/Fragment2.kt
+++ app/src/main/java/com/like/common/sample/fragmenttest/Fragment2.kt
@@ -10,6 +10,7 @@ import android.widget.TextView
 
 class Fragment2 : LogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
         return TextView(context).apply {
             layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
             gravity = Gravity.CENTER
