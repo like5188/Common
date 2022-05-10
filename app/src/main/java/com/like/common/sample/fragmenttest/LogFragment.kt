cd433177420111b332d2046322e7ac0f67cf1190
like5188 <260864866@qq.com>
2022-05-10 10:25:07 +0800

    完善例子

--- app/src/main/java/com/like/common/sample/fragmenttest/LogFragment.kt
+++ app/src/main/java/com/like/common/sample/fragmenttest/LogFragment.kt
@@ -1,6 +1,7 @@
 package com.like.common.sample.fragmenttest
 
 import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -13,13 +14,13 @@ open class LogFragment : BaseLazyFragment() {
     private var TAG = javaClass.simpleName
 
     override fun onAttach(context: Context) {
-        Log.w(TAG, "onAttach")
         super.onAttach(context)
+        Log.w(TAG, "onAttach")
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.w(TAG, "onCreate")
         super.onCreate(savedInstanceState)
+        Log.w(TAG, "onCreate")
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -28,58 +29,73 @@ open class LogFragment : BaseLazyFragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        Log.w(TAG, "onViewCreated")
         super.onViewCreated(view, savedInstanceState)
+        Log.w(TAG, "onViewCreated")
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-        Log.w(TAG, "onActivityCreated")
         super.onActivityCreated(savedInstanceState)
+        Log.w(TAG, "onActivityCreated")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.w(TAG, "onSaveInstanceState")
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        Log.w(TAG, "onViewStateRestored")
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        Log.w(TAG, "onConfigurationChanged")
     }
 
     override fun onStart() {
-        Log.w(TAG, "onStart")
         super.onStart()
+        Log.w(TAG, "onStart")
     }
 
     override fun onResume() {
-        Log.e(TAG, "onResume")
         super.onResume()
+        Log.e(TAG, "onResume")
     }
 
     override fun onPause() {
-        Log.i(TAG, "onPause")
         super.onPause()
+        Log.i(TAG, "onPause")
     }
 
     override fun onStop() {
-        Log.i(TAG, "onStop")
         super.onStop()
+        Log.i(TAG, "onStop")
     }
 
     override fun onDestroyView() {
-        Log.i(TAG, "onDestroyView")
         super.onDestroyView()
+        Log.i(TAG, "onDestroyView")
     }
 
     override fun onDetach() {
-        Log.i(TAG, "onDetach")
         super.onDetach()
+        Log.i(TAG, "onDetach")
     }
 
     override fun onDestroy() {
-        Log.i(TAG, "onDestroy")
         super.onDestroy()
+        Log.i(TAG, "onDestroy")
     }
 
     override fun setUserVisibleHint(isVisibleToUser: Boolean) {
-        Log.v(TAG, "setUserVisibleHint:isVisibleToUser-->$isVisibleToUser")
         super.setUserVisibleHint(isVisibleToUser)
+        Log.v(TAG, "setUserVisibleHint:isVisibleToUser-->$isVisibleToUser")
     }
 
     override fun onHiddenChanged(hidden: Boolean) {
-        Log.v(TAG, "onHiddenChanged:shown-->${!hidden}")
         super.onHiddenChanged(hidden)
+        Log.v(TAG, "onHiddenChanged:shown-->${!hidden}")
     }
 
     override fun onLazyLoadData() {
