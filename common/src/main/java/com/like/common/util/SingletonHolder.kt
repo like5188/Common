1927ce8eb4cf3e39994e844ae61ba8b370a01975
like <260864866@qq.com>
2023-03-28 09:42:42 +0800

    完善SingletonHolder.kt

--- common/src/main/java/com/like/common/util/SingletonHolder.kt
+++ common/src/main/java/com/like/common/util/SingletonHolder.kt
@@ -18,27 +18,61 @@ class SingletonHolderTest private constructor(val name: String, val age: Int) {
     })
 }
  */
-open class SingletonHolder<out R>(initializer: FunctionN<R>) {
+open class SingletonHolderN<out R>(initializer: FunctionN<R>) {
     private var initializer: FunctionN<R>? = initializer
+
     @Volatile
     private var instance: R? = null
 
-    fun getInstance(vararg args: Any?): R {
-        val instance1 = instance
-        if (instance1 != null) {
-            return instance1
-        }
-
-        return synchronized(this) {
-            val instance2 = instance
-            if (instance2 != null) {
-                instance2
-            } else {
-                val created = initializer!!.invoke(*args)
-                instance = created
-                initializer = null
-                created
+    fun getInstance(vararg args: Any?): R =
+        instance ?: synchronized(this) {
+            instance ?: initializer!!(*args).apply {
+                instance = this
             }
         }
-    }
+}
+
+open class SingletonHolder1<in I, out R>(initializer: (I) -> R) {
+    private var initializer: ((I) -> R)? = initializer
+
+    @Volatile
+    private var instance: R? = null
+
+    fun getInstance(arg: I): R =
+        instance ?: synchronized(this) {
+            instance ?: initializer!!(arg).apply {
+                instance = this
+            }
+        }
+
+}
+
+open class SingletonHolder2<in I1, in I2, out R>(initializer: (I1, I2) -> R) {
+    private var initializer: ((I1, I2) -> R)? = initializer
+
+    @Volatile
+    private var instance: R? = null
+
+    fun getInstance(arg1: I1, arg2: I2): R =
+        instance ?: synchronized(this) {
+            instance ?: initializer!!(arg1, arg2).apply {
+                instance = this
+            }
+        }
+
+}
+
+open class SingletonHolder3<in I1, in I2, in I3, out R>(initializer: (I1, I2, I3) -> R) {
+    private var initializer: ((I1, I2, I3) -> R)? = initializer
+
+    @Volatile
+    private var instance: R? = null
+
+    fun getInstance(arg1: I1, arg2: I2, arg3: I3): R =
+        instance ?: synchronized(this) {
+            instance ?: initializer!!(arg1, arg2, arg3).apply {
+                instance = this
+            }
+        }
+
 }
